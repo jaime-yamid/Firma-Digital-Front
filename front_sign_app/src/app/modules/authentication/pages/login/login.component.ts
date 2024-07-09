7c340b17c396c127eab63cf364f23f72873e83b3
Juan rojas <71771380+Juan-rojas75@users.noreply.github.com>
2024-07-08 22:30:09 -0500

    Fixed:login

--- front_sign_app/src/app/modules/authentication/pages/login/login.component.ts
+++ front_sign_app/src/app/modules/authentication/pages/login/login.component.ts
@@ -3,7 +3,6 @@ import { FormBuilder, FormGroup, Validators, } from '@angular/forms';
 import { Router} from '@angular/router';
 import { LoginService } from 'src/app/services/auth/login.service';
 import { AutenticacionService } from 'src/app/services/auth/autenticacion/autenticacion.service';
-import Swal from 'sweetalert2'
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -12,63 +11,49 @@ import Swal from 'sweetalert2'
 export class LoginComponent implements OnInit {
 
   loginForm!: FormGroup;
-  router:any;
+  // router:any;
 formLogin: any;
 
   constructor(
     private formBuilder: FormBuilder,
     private autenticacionservice: AutenticacionService,
-    private aService : AutenticacionService,
-    private ruta:Router) { }
+    private router:Router) { }
 
 
     ngOnInit(): void {
 
-      // aqui digo en donde el campo de validatos que con un correo se autentica 
+      // aqui digo en donde el campo de validatos que con un correo se autentica
       this.loginForm = this.formBuilder.group({
         user:['',[Validators.required, Validators.email]],
         password:['',[Validators.required, Validators.minLength(5)]],
       });
-  
+
     }
- 
 
 
-  get user(){
-    return this.loginForm.get('user');
-  
-  }
-get password(){
-  return this.loginForm.get('password');
-}
-  realizarlogin(){
+
+  // get user(){
+  //   return this.loginForm.get('user');
+  // }
+  // get password(){
+  //   return this.loginForm.get('password');
+  // }
+  login(){
     if(this.loginForm.invalid){
       return;
-    
+
     }
-    // agarro la informacion la envio en la data es la constante 
+    // agarro la informacion la envio en la data es la constante
       const data=this.loginForm.value;
-    
-    
-    this.autenticacionservice.login(data).subscribe({
-      next:(resp: any) => {
-        if ( resp && resp.usuario){
-          const {nombre,login,email} =  resp.usuario;
-          Swal.fire({
-            html: `Bienvenido ${nombre}`,
-          }).then(()=> {
-            this.ruta.navigateByUrl('home/dashboard');        
-          });
-          }
-        },
-        error:(error: any) => {
-          console.error(error.error.msg);
-        }
-      });
+    this.autenticacionservice.login(data).subscribe(
+      (response:any)=>{
+        console.log(response)
+        this.router.navigateByUrl('home/dashboard');
+      },
+      (err:any) =>{
+        console.error(err)
+      }
+    );
     }
-    
-
-
- 
 }
 
