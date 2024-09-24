791d87f6e241773ac2cb7be1ec8b9ee9a61c72ce
jaime-yamid <139189571+jaime-yamid@users.noreply.github.com>
2024-09-24 01:04:26 -0500

    despliegue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,22 +7,15 @@ import { AuthenticationModule } from './modules/authentication/authentication.mo
 import { HomeUserModule } from './modules/home-user/home-user.module';
 import { ComponentsModule } from './modules/components/components.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    FormsModule,
-    AuthenticationModule,
-    ComponentsModule,
-    HomeUserModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+@NgModule({ declarations: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule,
+        FormsModule,
+        AuthenticationModule,
+        ComponentsModule,
+        HomeUserModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
