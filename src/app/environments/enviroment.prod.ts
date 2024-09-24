fed3610521f13d910496867df666a7f954aeb9d4
jaime-yamid <139189571+jaime-yamid@users.noreply.github.com>
2024-09-24 01:14:24 -0500

    cambiando la parte de produccion

--- src/app/environments/enviroment.prod.ts
+++ src/app/environments/enviroment.prod.ts
@@ -1,6 +1,6 @@
 // aca se colcoas las IP o endpoint de las API que vo a consumir 
 export const environment = {
-    production:false,
+    production:true,
     base_url:"http://localhost:4000/api/v1",
 
 
